739166476406855d54df95d32679afe0aab56b7d
Isaac Gerard <IsaacBathSpa@gmail.com>
2024-05-05 14:51:52 +0400

    PageTransition.js

--- JavaScript/PageTransition.js
+++ JavaScript/PageTransition.js
@@ -1,25 +1,32 @@
-// This executes the code once the window is fully loaded.
+// This function gets executed when the window loads.
 window.onload = () => {
-  // This selects all of the anchor elements in the document.
-  const anchorElements = document.querySelectorAll('a');
+  // This selects all of the anchor elements in the document except the ones within the footer.
+  const anchorElements = document.querySelectorAll('a:not(.FooterAtBottom a)');
   // This selects the transition element with the class named "PageTransition".
   const transitionElement = document.querySelector('.PageTransition');
 
   //I set a setTimeout to remove the class named "ActiveState" from the transition element after 450 milliseconds.
   setTimeout(() => transitionElement.classList.remove('ActiveState'), 450);
 
-  // This iterates over each anchor element.
-  anchorElements.forEach(anchor => {
-    // I added a click event listener to all of the anchor element.
-    anchor.addEventListener('click', e => {
-      // I used this to prevent the default behavior of clicking on a link, which stops the browser from navigating to a new page.
-      e.preventDefault();
-      // This gets the target URL from the href attribute of the clicked anchor element.
-      const target = e.target.href;
-      // This adds the class named "ActiveState" to the transition element.
-      transitionElement.classList.add('ActiveState');
-       // I also set a setTimeout to change the window location to the target URL after 450 milliseconds.
-      setTimeout(() => window.location.href = target, 450);
+  // This selects all elements with the class named "More-Projects-ExploreButton-Content"
+  const exploreButtons = document.querySelectorAll('.More-Projects-ExploreButton-Content');
+
+  // This iterates over each "Explore" button element.
+  exploreButtons.forEach(exploreButton => {
+    // I added a click event listener to all of the "Explore" button elements.
+    exploreButton.addEventListener('click', e => {
+      // This determines if the anchor within the "Explore" button element already has a target attribute set to _blank.
+      const anchor = exploreButton.querySelector('a');
+      if (!anchor.getAttribute('target') || anchor.getAttribute('target') !== '_blank') {
+        // This prevents the default behavior only if the link doesn't already have a target="_blank".
+        e.preventDefault();
+        // This gets the target URL from the href attribute of the anchor element that is within the clicked "Explore" button.
+        const target = anchor.href;
+        // This adds the class named "ActiveState" to the transition element.
+        transitionElement.classList.add('ActiveState');
+        // This removes the class named "ActiveState" from the transition element after a short delay.
+        setTimeout(() => window.open(target, '_blank'), 450);
+      }
     });
   });
 };
